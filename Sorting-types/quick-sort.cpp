af89ece77c01933b3d2e868b608d0157b0c27698
DishantPatel300807 <dishantpatel2007308@gmail.com>
2026-08-31 10:45:31 +0530

    Adding comments to quick sort file

--- Sorting-types/quick-sort.cpp
+++ Sorting-types/quick-sort.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-int size() {
+int size() { // Getting the size of array from the user
     int n;
     cout << "Enter the size of array : ";
     cin >> n;
     return n;
 }
 
-void input(vector<int> &array) {
+void input(vector<int> &array) { // Getting the data of array from the user
     for (int i = 0; i < array.size(); i++)
     {        
         cout << "Enter the data : ";
@@ -22,12 +22,13 @@ void input(vector<int> &array) {
 int partition(vector<int> &array, int low, int high) {
     int pivot = array[high];
 
-    int i = low - 1;
+    int i = low - 1; //  Index of smaller elements
 
     for (int j = low; j <= high - 1; j++)
     {
         if (array[j] < pivot)
         {
+            // Swapping smaller elemets to the left side
             i++;
             int temp = array[j];
             array[j] = array[i];
@@ -35,6 +36,7 @@ int partition(vector<int> &array, int low, int high) {
         }
     }
     
+    // Placing pivot point to its correct position (final swap)
     int a = array[i+1];
     array[i+1] = array[high];
     array[high] = a;
@@ -44,14 +46,15 @@ int partition(vector<int> &array, int low, int high) {
 void quickSort(vector<int> &array, int low, int high) {
     if (low < high)
     {
-        int part = partition(array, low, high);
+        int part = partition(array, low, high); // Pivot index
 
-        quickSort(array, low, part - 1);
-        quickSort(array, part + 1, high);
+        quickSort(array, low, part - 1); // Sorting left side
+        quickSort(array, part + 1, high); // Sorting right side
     }
     
 }
 
+// Printing the array
 void printArray(vector<int> &array) {
     for (int i = 0; i < array.size(); i++)
     {
